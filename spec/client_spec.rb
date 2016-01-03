b9a0fecd1890b814a8c3d4eb3fcb4be108c25ae9
itkin <nicolas.papon@webflows.fr>
2016-01-03 22:09:40 +0100

    added vcr fake env

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,9 +2,8 @@ require 'spec_helper'
 
 describe Lemonway::Client do
   let(:opts){
-    # YAML.load_file('./config.yml')
     {
-      :wsdl     => "http://api.lemonway.com?wsdl",
+      :wsdl     => "https://ws.lemonway.fr/mb/ioio/dev/directkit/service.asmx?wsdl",
       :wlLogin  => "test",
       :wlPass   => "test",
       :wlPDV    => "test",
@@ -27,25 +26,25 @@ describe Lemonway::Client do
       subject.operations
     end
   end
-  # commented to pass with travis
-  # describe :api_methods do
-  #   let(:method_opts){
-  #     {
-  #       wallet: "123",
-  #       client_mail:        "nico@las.com",
-  #       client_first_name:  "nicolas",
-  #       client_last_name:   "nicolas"
-  #     }
-  #   }
-  #   it "are delegated to the client instance" do
-  #     expect(subject.instance).to receive(:operations).and_return([:register_wallet])
-  #     expect(subject.instance).to receive(:call).with(:register_wallet, :message => opts.update(method_opts).camelize_keys).and_call_original
-  #     VCR.use_cassette("create_wallet_success") do
-  #       resp = subject.register_wallet method_opts
-  #       expect(resp[:wallet][:id]).to eq "123"
-  #       expect(resp[:wallet][:lwid]).to eq "591"
-  #     end
-  #   end
-  # end
+
+  describe :api_methods do
+    let(:method_opts){
+      {
+        wallet: "123",
+        client_mail:        "nico@las.com",
+        client_first_name:  "nicolas",
+        client_last_name:   "nicolas"
+      }
+    }
+    it "are delegated to the client instance" do
+      expect(subject.instance).to receive(:operations).and_return([:register_wallet])
+      expect(subject.instance).to receive(:call).with(:register_wallet, :message => opts.update(method_opts).camelize_keys).and_call_original
+      VCR.use_cassette("create_wallet_success") do
+        resp = subject.register_wallet method_opts
+        expect(resp[:wallet][:id]).to eq "123"
+        expect(resp[:wallet][:lwid]).to eq "591"
+      end
+    end
+  end
 
 end
