a3da2a7f35725020ff6b43996ab5274b5619c9ec
itkin <nicolas.papon@webflows.fr>
2016-01-03 22:00:40 +0100

    make travis pass

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -2,17 +2,17 @@ require 'spec_helper'
 
 describe Lemonway::Client do
   let(:opts){
-    YAML.load_file('./config.yml')
-    # {
-    #   :wsdl     => "http://api.lemonway.com?wsdl",
-    #   :wlLogin  => "test",
-    #   :wlPass   => "test",
-    #   :wlPDV    => "test",
-    #   :language => "fr",
-    #   :version  => "1.0",
-    #   :channel  => "W",
-    #   :walletIp => "91.222.286.32"
-    # }
+    # YAML.load_file('./config.yml')
+    {
+      :wsdl     => "http://api.lemonway.com?wsdl",
+      :wlLogin  => "test",
+      :wlPass   => "test",
+      :wlPDV    => "test",
+      :language => "fr",
+      :version  => "1.0",
+      :channel  => "W",
+      :walletIp => "91.222.286.32"
+    }
   }
   subject { Lemonway::Client.new(opts) }
 
