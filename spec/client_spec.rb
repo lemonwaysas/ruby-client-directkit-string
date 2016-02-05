425a96662df2daf2caa6b9547c4fffa6e884f092
itkin <nicolas.papon@webflows.fr>
2016-02-05 15:17:49 +0100

    allow options to be passed to savon (change the public api)

--- spec/client_spec.rb
+++ spec/client_spec.rb
@@ -3,23 +3,41 @@ require 'spec_helper'
 describe Lemonway::Client do
   let(:opts){
     {
-      :wsdl     => "https://ws.lemonway.fr/mb/ioio/dev/directkit/service.asmx?wsdl",
       :wlLogin  => "test",
       :wlPass   => "test",
-      :wlPDV    => "test",
       :language => "fr",
       :version  => "1.0",
       :channel  => "W",
-      :walletIp => "91.222.286.32"
+      :walletIp => "91.222.286.32",
+      :wsdl     => "https://ws.lemonway.fr/mb/ioio/dev/directkit/service.asmx?wsdl"
     }
+    YAML.load_file('config.yml')
   }
   subject { Lemonway::Client.new(opts) }
+
+  describe :initialize do
+    it "needs one hash with :wsdl key" do
+      client = Lemonway::Client.new(opts)
+      expect(client.instance.globals[:ssl_verify_mode]).to eq nil
+    end
+    it "accepts an optional second hash for savon options"  do
+      client = Lemonway::Client.new(opts, ssl_verify_mode: :none)
+      expect(client.instance.globals[:ssl_verify_mode]).to eq :none
+    end
+    it "accepts an optional block for savon options"  do
+      client = Lemonway::Client.new(opts) do |savon_options|
+        savon_options.ssl_verify_mode :none
+      end
+      expect(client.instance.globals[:ssl_verify_mode]).to eq :none
+    end
+  end
 
   describe :instance do
     it "is a savon client instance " do
       expect(subject.instance).to be_a(Savon::Client)
     end
   end
+
   describe :operations do
     it "is delegated to the client instance" do
       expect(subject.instance).to receive :operations
@@ -30,8 +48,8 @@ describe Lemonway::Client do
   describe :api_methods do
     let(:method_opts){
       {
-        wallet: "123",
-        client_mail:        "nico@las.com",
+        wallet: "1234",
+        client_mail:        "nico1234@las.com",
         client_first_name:  "nicolas",
         client_last_name:   "nicolas"
       }
@@ -41,8 +59,8 @@ describe Lemonway::Client do
       expect(subject.instance).to receive(:call).with(:register_wallet, :message => opts.update(method_opts).camelize_keys).and_call_original
       VCR.use_cassette("create_wallet_success") do
         resp = subject.register_wallet method_opts
-        expect(resp[:wallet][:id]).to eq "123"
-        expect(resp[:wallet][:lwid]).to eq "591"
+        expect(resp[:wallet][:id]).to eq "1234"
+        expect(resp[:wallet][:lwid]).to eq "834"
       end
     end
   end
